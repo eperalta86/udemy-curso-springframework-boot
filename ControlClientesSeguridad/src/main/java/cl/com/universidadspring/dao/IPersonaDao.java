b8d5f36ae1b46e7f84b5ec7062ff31b73cd73212
Esteban Peralta <esteban.peralta.galarce@gmail.com>
2022-07-18 17:36:30 -0400

    spring security

---
+++ ControlClientesSeguridad/src/main/java/cl/com/universidadspring/dao/IPersonaDao.java
@@ -0,0 +1,10 @@
+package cl.com.universidadspring.dao;
+
+import org.springframework.data.repository.CrudRepository;
+import cl.com.universidadspring.dominio.Persona;
+
+//Ya no se utiliza @Repository, se implementa interfaz CrudRepository pasandole la clase y la llave primaria
+//Tampoco es necesario crear una implementación para esta clase ya que Spring Boot crea una implementación por defecto
+public interface IPersonaDao extends CrudRepository<Persona, Long> {
+
+}
